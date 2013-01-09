f719aed8c368daf79d6cc9941217313ea68e4966
Kyle Wilding <kwilding@nextgreatplace.com>
2013-01-08 17:59:40 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lawall::Application.config.secret_token = '256570909f00fb7cafb5324128ff1787eb1dc5e9b850f3a9893d89359eb4fe7b67a438864593d1b5271ae5f0318058057a5cabb68054d71ca504f8b217fab7a3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Lawall::Application.config.session_store :cookie_store, key: '_lawall_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Lawall::Application.config.session_store :active_record_store
